e0387995c322cc9ac7258a95bd79dbde86110d27
Lennart Braun <lenerd@posteo.de>
2016-01-12 00:51:51 +0100

    11.2

--- blatt_11/aufgabe-11.2.a.r
+++ blatt_11/aufgabe-11.2.a.r
@@ -16,8 +16,9 @@ hitOrMiss <- function(N) {
     cat("Punktschaetzung Pi/4:", hits/N, "\n")
     cat("Somit fuer Pi:", hits/N*4, "\n")
     cat("Konfidenzintervall:\n")
-    binom.test(hits, N, 3.14159/4, conf.level=0.9)$conf.int
+    binom.test(hits, N, conf.level=0.9)$conf.int
 }
 
 hitOrMiss(100)
 hitOrMiss(1000)
+hitOrMiss(100000)
--- blatt_11/aufgabe-11.2.b.r
+++ blatt_11/aufgabe-11.2.b.r
@@ -1,24 +1,20 @@
 #!/usr/bin/env Rscript
 
-hitOrMiss <- function(N) {
+crude <- function(N) {
 
-    x <- runif(N,0,1)
-    y <- runif(N,0,1)
-   
-    hits <- 0
 
-    for (i in 1:N) {
-        expected <- sqrt(1-x[i]^2)
-        if (y[i] <= expected) {
-            hits <- hits + 1
-        }
-    }
+    x <- runif(N, 0, 1)
+    y <- sqrt(1-x^2)
+    T <- sum(y) / N
+
+
     cat("\nErgebnisse fuer N =", N, "\n")
-    cat("Punktschaetzung Pi/4:", hits/N, "\n")
-    cat("Somit fuer Pi:", hits/N*4, "\n")
+    cat("Punktschaetzung Pi/4:", T, "\n")
+    cat("Somit fuer Pi:", T*4, "\n")
     cat("Konfidenzintervall:\n")
-    binom.test(hits, N, 3.14159/4, conf.level=0.9)$conf.int
+    t.test(y, conf.level=0.9)$conf.int
 }
 
-hitOrMiss(100)
-hitOrMiss(1000)
+crude(100)
+crude(1000)
+crude(100000)
